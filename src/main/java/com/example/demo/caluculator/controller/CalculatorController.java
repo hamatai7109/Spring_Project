da653e3165561b758c7b88cd4f5ee9d900a5f9ff
hamatai <taishihamano0515@gmail.com>
2023-10-04 19:57:47 +0900

    fixed calculator

--- src/main/java/com/example/demo/caluculator/controller/CalculatorController.java
+++ src/main/java/com/example/demo/caluculator/controller/CalculatorController.java
@@ -6,30 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.caluculator.service.CalculatorService;
+import com.example.demo.caluculator.service.CalculatorService.Result;
 
 @Controller
 public class CalculatorController {
 	private final CalculatorService calculatorService;
-	
+
 	public CalculatorController(CalculatorService calculatorService) {
 		this.calculatorService = calculatorService;
 	}
-	
+
 	@GetMapping("/calculator")
 	public String calculator(
 			@RequestParam(name = "firstNum", required = false) Integer firstNum,
 			@RequestParam(name = "secondNum", required = false) Integer secondNum,
 			@RequestParam(name = "operator", required = false) String operator,
 			Model model) {
-			String result = "";
-			String errorMessage = ""; 
-		    if (firstNum == null || secondNum == null || operator == null) {
-		        errorMessage = "値を入れてください";
-		    } else {
-		        result = String.valueOf(calculatorService.calculator(firstNum, secondNum, operator));
-		    }
-	        model.addAttribute("result", result);    
-	        model.addAttribute("errorMessage", errorMessage);
+		String result = "";
+		String errorMessage = null;
+
+		if (firstNum != null && secondNum != null && operator != null) {
+			Result calculationResult = calculatorService.calculator(firstNum, secondNum, operator);
+			if (calculationResult.getErrorMessage() != null) {
+				errorMessage = calculationResult.getErrorMessage();
+			} else {
+				result = String.valueOf(calculationResult.getResult());
+			}
+		}
+		model.addAttribute("result", result);
+		model.addAttribute("errorMessage", errorMessage);
 		return "calculator";
 	}
 }
--- src/main/java/com/example/demo/caluculator/service/CalculatorService.java
+++ src/main/java/com/example/demo/caluculator/service/CalculatorService.java
@@ -3,12 +3,14 @@ package com.example.demo.caluculator.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorService { 
-	public int calculator(int firstNum, int secondNum, String operator) {
+public class CalculatorService {
+	public Result calculator(Integer firstNum, Integer secondNum, String operator) {
 		int result = 0;
-		switch(operator) {
+		String errorMessage = null;
+
+		switch (operator) {
 		case "add":
-			result =  firstNum + secondNum;
+			result = firstNum + secondNum;
 			break;
 		case "subtract":
 			result = firstNum - secondNum;
@@ -18,13 +20,33 @@ public class CalculatorService {
 			break;
 		case "divide":
 			if (secondNum != 0) {
-                result = firstNum / secondNum;
-            } else {
-                throw new ArithmeticException("0で割ることはできません。");
-            }
-            break;
-        default:
-        	throw new ArithmeticException("演算子が正しく選択されていません。");		}
-		return result;
+				result = firstNum / secondNum;
+			} else {
+				errorMessage = "0で割ることはできません。";
+			}
+			break;
+		default:
+			errorMessage = "演算子が正しく選択されていません。";
+		}
+
+		return new Result(result, errorMessage);
+	}
+
+	public class Result {
+		private final int result;
+		private final String errorMessage;
+
+		public Result(int result, String errorMessage) {
+			this.result = result;
+			this.errorMessage = errorMessage;
+		}
+
+		public int getResult() {
+			return result;
+		}
+
+		public String getErrorMessage() {
+			return errorMessage;
+		}
 	}
 }
